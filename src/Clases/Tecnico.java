aa1db0aa28600ed4c8b806bf553f3b87bfaff974
TuKK2 <luchogonzalez2004@gmail.com>
2025-02-14 11:10:18 -0300

    Se agrego metodo obtenerTecnico a la clase Tecnico

--- src/Clases/Tecnico.java
+++ src/Clases/Tecnico.java
@@ -5,6 +5,12 @@
  */
 package Clases;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import modelos.dbConnection;
+
 /**
  *
  * @author TuKK
@@ -40,4 +46,35 @@ public class Tecnico extends Usuario {
         this.marcas = marcas;
     }
 
+    public static AuthResponse obtenerTecnico(int id) {
+        String sql = "SELECT u.*, t.fallas, t.marcas "
+                + "FROM usuarios u "
+                + "LEFT JOIN tecnicos t ON u.usuario_id = t.usuario_id "
+                + "WHERE u.usuario_id = ?";
+
+        try (Connection conn = dbConnection.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            //El usuario no se encontro
+            if (!rs.next()) {
+                return new AuthResponse(null, Mensaje.USUARIO_NO_ENCONTRADO);
+            }
+            String tipo = rs.getString("tipo");
+            //El usuario no es de tipo Tecnico
+            if (!tipo.toLowerCase().equals("tecnico")) {
+                return new AuthResponse(null, Mensaje.TIPO_DE_USUARIO_INVALIDO);
+            }
+            int fallas = rs.getInt("fallas");
+            int marcas = rs.getInt("marcas");
+
+            Usuario usuario = new Tecnico(rs.getString("nombre"), rs.getString("dni"), rs.getInt("legajo"), rs.getString("contrasena"), rs.getString("estado"), fallas, marcas);
+
+            return new AuthResponse(usuario, Mensaje.EXITO);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new AuthResponse(null, Mensaje.ERROR_CONEXION);
+        }
+    }
 }
